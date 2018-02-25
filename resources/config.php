07166b1a75534f4ddc57422604d323750daf5ff2
William del Rosario <williamdelrosario@yahoo.com>
2018-02-25 08:00:33 +0100

    Creating the category table.

--- resources/config.php
+++ resources/config.php
@@ -21,7 +21,7 @@ defined("DB_USER") ? null : define("DB_USER","root");
 
 defined("DB_PASS") ? null : define("DB_PASS", "root");
 
-defined("DB_NAME") ? null : define("DB_NAME",  "ecom2");
+defined("DB_NAME") ? null : define("DB_NAME",  "ecom_db");
 
 
 
