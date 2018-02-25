6e8bbb64e82b0a517e174cf51543b570cdb25a3d
William del Rosario <williamdelrosario@yahoo.com>
2018-02-25 08:33:07 +0100

    Added custom helper functions.

--- resources/functions.php
+++ resources/functions.php
@@ -1,9 +1,37 @@
 <?php
 
-//
-//if($connection)
-//{
-//    echo "is connected";
-//}
-//
-//echo "from functions";
+function redirect($location)
+{
+    header("Location: $location");
+}
+
+function query($sql)
+{
+    global $connection;
+    return mysqli_query($connection, $sql);
+}
+
+function confirm($result)
+{
+    global $connection;
+
+    if(!$result)
+    {
+        die("QUERY FAILED " . mysqli_error($connection));
+    }
+
+}
+
+function escape_string($string)
+{
+    global $connection;
+    return mysqli_real_escape_string($connection, $string);
+}
+
+function fetch_array($result)
+{
+    global $connection;
+    return mysqli_fetch_array($result);
+}
+
+
